d24507851b2ea7f6a4a7a303bf2e2097e614e173
nicole pank <nicole.pank@gmail.com>
2013-09-11 11:42:11 -0500

    Added default topping of cheese for no other toppings passed in

--- pizza.rb
+++ pizza.rb
@@ -5,16 +5,17 @@ module Pizza
     # Instantiate a new pizza.
     #
     # toppings   - The array listing the toppings
-    #              of the pizza.
+    #              of the pizza, and defaults to 
+    #              cheese if no other toppings.
     #
     # Returns a new pizza object.
-    def initialize(toppings)
+    def initialize(toppings = [Topping.new("cheese")])
       @toppings = toppings
     end
   end
 
   class Topping
-    attr_accessor :name, :vegetarian
+    attr_accessor :name, :vegetarian 
 
     # Instantiate a new topping.
     #
